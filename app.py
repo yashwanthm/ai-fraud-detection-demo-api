09cd9809ac5b5060b52e659c7d2b66df1a9cf816
Yashwanth Maheshwaram <yashwanth@redhat.com>
2024-08-08 12:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,32 +2,26 @@ import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import pickle
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
 
-@app.after_request
-def apply_cors(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    return response
-
+# Allow CORS for all domains on all routes
+CORS(app)
 
 # Read the environment variable
 ENV = os.getenv('FLASK_ENV', 'production')  # Default to 'development' if not set
 
-deployed_model_name = "fraud-detection" # Ensure that this is same as the model name you gave on OpenShift AI
+deployed_model_name = "fraud-detection"  # Ensure this is the same as the model name you gave on OpenShift AI
 if ENV == 'development':
-    rest_url = "https://fraud-ymaheshw-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com" #For local and locations other than the same OpenShift Instance
+    rest_url = "https://fraud-ymaheshw-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com"  # For local and locations other than the same OpenShift Instance
 else:
-    rest_url = "http://modelmesh-serving:8008" #if the api is deployed to the same OpenShift Instance
+    rest_url = "http://modelmesh-serving:8008"  # If the API is deployed to the same OpenShift Instance
 
 # Construct the inference URL
-infer_url = f"{rest_url}/v2/models/{deployed_model_name}/infer" 
+infer_url = f"{rest_url}/v2/models/{deployed_model_name}/infer"
 
-#Load the scaler.pkl that contained pre-trained scikit-learn scaler used to standardize or normalize input data. This ensures that the data fed into your model during inference is scaled consistently with how it was during training, improving model accuracy and performance
-import pickle
+# Load the scaler.pkl that contains the pre-trained scikit-learn scaler
 with open('artifact/scaler.pkl', 'rb') as handle:
     scaler = pickle.load(handle)
 
@@ -51,14 +45,13 @@ def rest_request(data):
 @app.route('/', methods=['POST'])
 def check_fraud():
     data = request.json
-    prediction = rest_request(scaler.transform([data]).tolist()[0]) # place a request to the model server from this service
-    threshhold = 0.95
-    if (prediction[0] > threshhold):
+    prediction = rest_request(scaler.transform([data]).tolist()[0])  # Place a request to the model server from this service
+    threshold = 0.95
+    if prediction[0] > threshold:
         message = 'fraud'
     else:
         message = 'not fraud'
     return jsonify({'message': message})
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
